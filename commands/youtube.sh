98fff7a1121c8619d01949d2a1d128f5966c35a3
andriesbron <andries@inourplace.nl>
2019-04-13 15:07:36 +0200

    Create youtube.sh

---
+++ commands/youtube.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Ffmbash is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Ffmbash is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Ffmbash.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Taken from https://gist.github.com/olasd/9841772
+# to be worked out yet...
+#@fahhu, to stream from webcam, set the SOURCE to this
+#SOURCE='/dev/video0'
+
+VBR="2500k"                                    # Bitrate de la vidéo en sortie
+FPS="30"                                       # FPS de la vidéo en sortie
+QUAL="medium"                                  # Preset de qualité FFMPEG
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"  # URL de base RTMP youtube
+
+SOURCE="udp://239.255.139.0:1234"              # Source UDP (voir les annonces SAP)
+KEY="...."                                     # Clé à récupérer sur l'event youtube
+
+ffmpeg \
+    -i "$SOURCE" -deinterlace \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
+    -f flv "$YOUTUBE_URL/$KEY"
+
