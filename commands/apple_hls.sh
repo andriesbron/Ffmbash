b96b9998dbaa71d9f3f8ae56ff67522fd08a7c3f
andriesbron <andries@inourplace.nl>
2019-04-15 10:59:23 +0200

    Update apple_hls.sh
    closes #3

--- commands/apple_hls.sh
+++ commands/apple_hls.sh
@@ -13,4 +13,4 @@
 # You should have received a copy of the GNU General Public License
 # along with Ffmbash.  If not, see <http://www.gnu.org/licenses/>.
 
-COMMAND="ffmpeg -y ${ffmbashfpsin} -f avfoundation -i \"${ff_vdev}:${ff_adev}\" -pix_fmt yuv420p -s ${ff_screen_resolution} -hls_flags round_durations -hls_time 3 -hls_init_time 3 -threads 25 -vcodec libx264 -ar 44100 -ab 128 -af aresample=async=1000 \"${ff_rootdir}/${output_dir}/test.m3u8\""
+COMMAND="ffmpeg -y ${ffmbashfpsin} -f avfoundation -i \"${ff_vdev}:${ff_adev}\" -pix_fmt yuv420p -s ${ff_screen_resolution} -hls_flags round_durations -hls_time 3 -threads 25 -vcodec libx264 -ar 44100 -ab 128 -af aresample=async=1000 \"${ff_rootdir}/${output_dir}/test.m3u8\""
